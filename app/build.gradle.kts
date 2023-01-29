473da052403793e0ee81eb7f543f31f45ee0056c
Dmitry <dmitrijkotov634@gmail.com>
2023-01-29 21:08:04 +0300

    feat: new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,9 +10,9 @@ android {
     defaultConfig {
         applicationId = "com.wavecat.mivlgu"
         minSdk = 21
-        targetSdk = 32
-        versionCode = 2
-        versionName = "0.3"
+        targetSdk = 33
+        versionCode = 4
+        versionName = "0.5"
     }
 
     buildTypes {
@@ -45,11 +45,11 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.8.0-alpha01")
+    implementation("androidx.appcompat:appcompat:1.6.0")
+    implementation("com.google.android.material:material:1.9.0-alpha01")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel:2.5.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
     implementation("com.github.bumptech.glide:glide:4.13.2")
